199dd809f5292fdad35110c902821e1e994b3e69
Myriam <myriam-gewinnt@hotmail.com>
2015-09-23 13:13:32 -0600

    factorial

---
+++ scripts/myscripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+function facto(no, resultado){
+		if (no==0 || no==1){
+		resultado= 1;
+		return resultado;
+		}else{
+			while ( no > 1 ){
+				resultado = resultado*(no-1);
+				no= no-1;
+				} return resultado;
+		 }
+}
+
+$("form").on("submit",function(event){
+	event.preventDefault();
+
+	var no= parseInt($("input#numero").val());
+	var resultado= parseInt($("input#numero").val());
+	var result = facto(no, resultado);
+	console.log(result);
+
+		$("#tabla").append("<tr><td>" + no + "</td><td>" + result + "</td></tr>");
+		})
+	})
+
